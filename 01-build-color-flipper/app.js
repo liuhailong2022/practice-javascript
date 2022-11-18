2a15afe36e5e0a52ba80fdd1812b413c038fd8c5
liuhailong2022 <liuhailong2022@gmail.com>
2022-11-18 14:57:40 -0500

    added 01-build-color-flipper

---
+++ 01-build-color-flipper/app.js
@@ -0,0 +1,16 @@
+const colors = ["red", "green", "blue", "yellow", "black"]
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+btn.addEventListener("click", () => {
+
+    // get a random number
+    const randomNumber = getRandomNumber()
+
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
